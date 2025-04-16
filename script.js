39d9d25d9e3a100793ce44b73a29df8834129b15
Joziberto <jozibertoalvesteixeirafilho@gmail.com>
2025-04-16 02:20:37 -0300

    feat: enhance theme toggle functionality for contact buttons

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const body = document.body;
 const navbar = document.querySelector('.navbar');
 const heroSection = document.querySelector('.hero');
 const button =  document.getElementById('sobreLink');
+const contactButton = document.getElementById('social-links');
+const contactButton1 = document.getElementById('social-links1');
+const contactButton2 = document.getElementById('social-links2');
+const contactButton3 = document.getElementById('social-links3');
+const contactButton4 = document.getElementById('social-links4');
 
 // Define o estilo inicial
 heroSection.style.backgroundImage = "url('fundo2.png')";
@@ -16,7 +21,16 @@ themeToggle.addEventListener('click', () => {
     navbar.classList.toggle('bg-primary');
     button.classList.toggle('btn-outline-light')
     button.classList.toggle('btn-outline-dark')
-    
+    contactButton.classList.toggle('btn-outline-light');
+    contactButton.classList.toggle('btn-outline-dark');
+    contactButton1.classList.toggle('btn-outline-light');
+    contactButton1.classList.toggle('btn-outline-dark');
+    contactButton2.classList.toggle('btn-outline-light');
+    contactButton2.classList.toggle('btn-outline-dark');
+    contactButton3.classList.toggle('btn-outline-light');
+    contactButton3.classList.toggle('btn-outline-dark');
+    contactButton4.classList.toggle('btn-outline-light');
+    contactButton4.classList.toggle('btn-outline-dark');
 
     const icon = themeToggle.querySelector('i');
     if (body.classList.contains('bg-dark')) {
